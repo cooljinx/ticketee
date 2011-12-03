c9956c6f7e04856b05ba70a2869f5df00d5ca780
Gary <cooljinx@gmail.com>
2011-12-03 11:41:16 +0530

    Added feature for signing in via the Devise-provided form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,21 @@ Ticketee::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+	# A dummy setup for development
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.default :charset => "utf-8"
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'baci.lindsaar.net',
+  :user_name            => 'cooljinx',
+  :password             => '13fingers8',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
